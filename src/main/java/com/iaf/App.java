bb6f309b0c32072ccfb88b88c7b7e314a534db62
Chidambaram07 <143097368+Chidambaram07@users.noreply.github.com>
2026-02-28 11:08:54 +0530

    Add files via upload

---
+++ src/main/java/com/iaf/App.java
@@ -0,0 +1,13 @@
+package com.iaf;
+
+public class App {
+
+    public static String getMotto() {
+        return "Touch The Sky With Glory";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Indian Air Force");
+        System.out.println(getMotto());
+    }
+}
